1d8b874e561d6384894c2e9062850005d9eee55e
Ricardo Chavez <74266998+ExHultant@users.noreply.github.com>
2024-04-15 22:42:46 -0400

    Refactor user list page to fetch data from server in +page.svelte

--- last-project-v2/src/routes/users/+page.svelte
+++ last-project-v2/src/routes/users/+page.svelte
@@ -1,6 +1,12 @@
 <script>
-    export let data;
-    console.log(data);
+    import { onMount } from 'svelte';
+    import { page } from '$app/stores';
+    let users = [];
+
+    onMount(async () => {
+        const { users: serverUsers } = await $page.data;
+        users = serverUsers;
+    });
 </script>
 
 <div class="px-4 sm:px-6 lg:px-8">
@@ -30,16 +36,17 @@
                             </tr>
                         </thead>
                         <tbody class="divide-y divide-gray-200 bg-white">
-                            <tr>
-                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">Lindsay Walton</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">lindsay.walton@example.com</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
-                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
-                                </td>
-                            </tr>
-                            <!-- More people... -->
+                            {#each users as user (user.id)}
+                                <tr>
+                                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{user.name}</td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.title}</td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.email}</td>
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.role}</td>
+                                    <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                        <a href="/edit/{user.id}" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, {user.name}</span></a>
+                                    </td>
+                                </tr>
+                            {/each}
                         </tbody>
                     </table>
                 </div>
